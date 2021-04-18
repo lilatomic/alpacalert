23b91bfb4e5f518640f74d272f9bf8d17992aa14
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-04-18 00:06:13 -0400

    add polling to console_autowired_grafana

--- src/examples/scala/console_autowired_grafana.scala
+++ src/examples/scala/console_autowired_grafana.scala
@@ -3,12 +3,17 @@ import ca.lilatomic.alpacalert.{BasicService, SystemSeq}
 import java.io.IOException
 import zio.{ZIO, URIO, Has}
 import zio.console._
+import zio.clock._
+import zio.Schedule
+import zio._
+import zio.duration.durationInt
 import ca.lilatomic.alpacalert.sensors.GrafanaConnection
 import ca.lilatomic.alpacalert.visualisers.ConsoleVisualiser
 
 /**
  * This example shows how to quickly query which grafana dashboards are alerting.
- *
+ * It assumes that each dashboard represents its own Service where all items are required
+ * and creates the hierarchy to represent this
  */
 object console_autowired_grafana extends zio.App {
 	val program: ZIO[Console with GrafanaConnection, Throwable, Unit] = for {
@@ -18,8 +23,15 @@ object console_autowired_grafana extends zio.App {
 		_ <- putStrLn(services.map(ConsoleVisualiser.visualise(_)).mkString(""))
 	} yield ()
 
-	def run(args: List[String]) =
+	/**
+	 * This schedule will query every 10 seconds, forever.
+	 */
+	val schedule = Schedule.fixed(10.seconds) && Schedule.forever
+
+	def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] = {
 		program
-			.provideLayer(GrafanaConnection.demoGrafana ++ Console.live)
+			.provideCustomLayer(GrafanaConnection.demoGrafana)
+			.repeat(schedule)
 			.exitCode
+	}
 }
