165843c8fdd0e0309da7b9cbc082a440707f9eb4
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-04-20 02:58:58 -0400

    add auth to GrafanaConnection

--- src/main/scala/ca/lilatomic/alpacalert/sensors/GrafanaSensor.scala
+++ src/main/scala/ca/lilatomic/alpacalert/sensors/GrafanaSensor.scala
@@ -33,23 +33,8 @@ case class GrafanaAlert
 type GrafanaConnection = Has[GrafanaConnection.Service]
 
 object GrafanaConnection {
-	val fromConfig: ZLayer[Has[GrafanaConnectionConfig], Nothing, GrafanaConnection] = ZLayer.fromService { cfg =>
-		new Service {
-			val request = basicRequest.get(cfg.url).response(asJson[List[GrafanaAlert]])
-			val backend = HttpURLConnectionBackend()
-
-			override def getAlerts() = {
-				val response = request.send(backend).body
-				response match {
-					case Left(e) => Task.fail(e)
-					case Right(l) => {
-						val sensors = l.map(alert2sensor(_))
-						val sensorsById = sensors.map(e => (e.id, e)).toMap
-						Task.succeed(sensorsById)
-					}
-				}
-			}
-		}
+	val fromConfig: ZLayer[Has[GrafanaConnectionConfig], Nothing, GrafanaConnection] = ZLayer.fromService {
+		ServiceImpl(_)
 	}
 
 	val demoGrafana: ZLayer[Any, Nothing, GrafanaConnection] = {
@@ -63,6 +48,30 @@ object GrafanaConnection {
 	trait Service {
 		def getAlerts(): Task[Map[Integer, GrafanaSensor]]
 		//		def getSensorById(id: Integer): Option[Sensor]
+	}
+
+	class ServiceImpl(cfg: GrafanaConnectionConfig) extends Service {
+		val request = basicRequest.get(cfg.url).response(asJson[List[GrafanaAlert]])
+		val backend = HttpURLConnectionBackend()
+
+		override def getAlerts() = {
+			val request_auth = cfg.auth match {
+				case auth: GrafanaConnectionConfig.AuthBasic => request.auth.basic(auth.user, auth.password)
+				case auth: GrafanaConnectionConfig.AuthToken => request.auth.bearer(auth.token)
+				case _: GrafanaConnectionConfig.AuthNone.type => request
+			}
+
+			val response = request_auth.send(backend).body
+
+			response match {
+				case Left(e) => Task.fail(e)
+				case Right(l) => {
+					val sensors = l.map(alert2sensor(_))
+					val sensorsById = sensors.map(e => (e.id, e)).toMap
+					Task.succeed(sensorsById)
+				}
+			}
+		}
 	}
 }
 
