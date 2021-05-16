e9e420b36a255418ffcb042bad8b86b1bf74661c
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-05-16 02:18:07 -0400

    [1] propagate ZIO through System and Service

--- src/main/scala/ca/lilatomic/alpacalert/Service.scala
+++ src/main/scala/ca/lilatomic/alpacalert/Service.scala
@@ -1,12 +1,13 @@
 package ca.lilatomic.alpacalert
 
 import ca.lilatomic.alpacalert.System
+import zio.UIO
 
 /**
  * a service with some basic metadata
  */
 class BasicService(val name: String, val system: System) extends Service() {
-	override def status(): Status = system.status()
+	override def status(): UIO[Status] = system.status()
 
 	override def children(): Seq[Scanner] = Seq(system)
 }
--- src/main/scala/ca/lilatomic/alpacalert/System.scala
+++ src/main/scala/ca/lilatomic/alpacalert/System.scala
@@ -1,15 +1,20 @@
 package ca.lilatomic.alpacalert
 
 import ca.lilatomic.alpacalert.Sensor
+import zio.{UIO, ZIO}
 
 class SystemPar(val name: String, val sensors: Seq[Sensor]) extends System() {
-	override def status(): Status = sensors.map(_.status()).reduce(Status.|)
+	override def status(): UIO[Status] = ZIO.collectAll(
+		sensors.map(_.status())
+	).map(_.reduce(Status.|))
 
 	override def children(): Seq[Sensor] = sensors
 }
 
 class SystemSeq(val name: String, val sensors: Seq[Sensor]) extends System() {
-	override def status(): Status = sensors.map(_.status()).reduce(Status.&)
+	override def status(): UIO[Status] = ZIO.collectAll(
+		sensors.map(_.status())
+	).map(_.reduce(Status.&))
 
 	override def children(): Seq[Sensor] = sensors
 }
