2956a0ef39f25238f2b6461bf224693ae888e328
Daniel Goldman <danielgoldman4@gmail.com>
2024-06-03 23:18:01 -0400

    allow changing characters of VisualiserConsole

--- src/main/python/alpacalert/visualisers/console.py
+++ src/main/python/alpacalert/visualisers/console.py
@@ -1,10 +1,22 @@
 import itertools
+from dataclasses import dataclass, field
 
-from alpacalert.models import Log, Scanner, Service, Visualiser
+from alpacalert.models import Log, Scanner, Service, Visualiser, Status, State
 
 
+def mk_symbols(passing: str, failing: str, unknown: str):
+	return {
+		State.PASSING: passing,
+		State.FAILING: failing,
+		State.UNKNOWN: unknown,
+	}
+
+
+@dataclass
 class VisualiserConsole(Visualiser):
 	"""Visualise an alpacalert Service to the console"""
+
+	symbols: dict[State, str] = field(default_factory=lambda: mk_symbols(State.PASSING.value, State.FAILING.value, State.UNKNOWN.value))
 
 	def visualise(self, service: Service):
 		"""Visualise an alpacalert Service"""
@@ -17,7 +29,7 @@ class VisualiserConsole(Visualiser):
 	def _visualise_scanner(self, scanner: Scanner, indent: int) -> list[str]:
 		indent_s = "\t" * indent
 		status = scanner.status()
-		this = f"{indent_s}{status.state.value} : {scanner.name}"
+		this = f"{indent_s}{self.symbols[status.state]} : {scanner.name}"
 		logs = [self._visualise_log(log, indent) for log in status.messages]
 		children = [self._visualise_scanner(e, indent + 1) for e in scanner.children()]
 
