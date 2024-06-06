8f6e33a0805e9a21865266e985d0c50af86c14e2
Daniel Goldman <danielgoldman4@gmail.com>
2024-06-06 02:18:46 -0400

    add grafana instrumentors and sensors

---
+++ src/main/python/alpacalert/instrumentors/grafana.py
@@ -0,0 +1,235 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import ClassVar, Iterable, TypeVar
+
+import requests
+from cachetools import TTLCache, cached
+
+from alpacalert.generic import status_all
+from alpacalert.instrumentor import Instrumentor, InstrumentorError, InstrumentorRegistry, Kind, Registrations
+from alpacalert.instrumentors import grafana_models as m
+from alpacalert.models import Log, Scanner, Sensor, Severity, State, Status, System, flatten
+
+T = TypeVar("T")
+
+
+@dataclass(frozen=True)
+class GrafanaObjRef:
+	name: str
+	p: dict[str, str] = field(default_factory=dict)
+
+
+# org: int | str # TODO
+
+
+@dataclass
+class GrafanaApi:
+	base_url: str
+	session: requests.Session
+
+	def call(self, req: requests.Request):
+		return self.session.send(self.session.prepare_request(req))
+
+	@cached(cache=TTLCache(maxsize=1, ttl=60), key=lambda s: None)
+	def request_alertgroups(self) -> list[m.Group]:
+		res = self.call(requests.Request("GET", self.base_url + "/api/prometheus/grafana/api/v1/rules"))
+		if not res.ok:
+			raise InstrumentorError(res)
+
+		return m.GrafanaAlertsResponse.model_validate_json(res.content).data.groups
+
+	def index_alertgroups(self, groups: list[m.Group]) -> dict[str, dict[str, tuple[m.Group, m.Rule]]]:
+		return {e.name: {f.name: (e, f) for f in e.rules} for e in groups}
+
+	def by_name(self, obj: Iterable[T]) -> dict[str, T]:
+		return {e.name: e for e in obj}
+
+	def get_group(self, group: str) -> m.Group:
+		group_names = self.by_name(self.request_alertgroups())
+
+		if group not in group_names:
+			raise InstrumentorError(f"alert group not found {group=}")
+		return group_names[group]
+
+	def get_rule(self, group: str, name: str) -> m.Rule:
+		group = self.get_group(group)
+		rules_by_name = self.by_name(group.rules)
+
+		if name not in rules_by_name:
+			raise InstrumentorError(f"rule not found {group=}, {name=}")
+		return rules_by_name[name]
+
+
+@dataclass
+class SensorAlert(Sensor):
+	"""Sensor for a Grafana alert"""
+
+	alert: m.Alert
+	state_when_pending: State = State.PASSING
+
+	kind: ClassVar[Kind] = Kind("grafana.org/alerts", "alert")
+
+	@property
+	def name(self) -> str:
+		return self.alert.name()
+
+	def status(self) -> Status:
+		match self.alert.state:
+			case m.AlertState.normal:
+				state = State.PASSING
+			case m.AlertState.alerting | m.AlertState.alerting_nodata | m.AlertState.alerting_error:
+				state = State.FAILING
+			case m.AlertState.error | m.AlertState.normal_error:
+				state = State.FAILING
+			case m.AlertState.pending:
+				state = self.state_when_pending
+			case m.AlertState.nodata:
+				state = State.UNKNOWN
+
+		match state:
+			case State.PASSING:
+				severity = Severity.INFO
+			case State.FAILING:
+				severity = Severity.ERROR
+			case State.UNKNOWN:
+				severity = Severity.WARN
+
+		return Status(state=state, messages=[Log(message=self.alert.state.value, severity=severity)])
+
+	def children(self) -> list[Scanner]:
+		return []
+
+
+@dataclass
+class InstrumentorAlert(Instrumentor):
+	api: GrafanaApi
+
+	def registrations(self) -> Registrations:
+		return [
+			(SensorAlert.kind, self),
+		]
+
+	def instrument(self, registry: InstrumentorRegistry, kind: Kind, **kwargs) -> list[Scanner]:
+		return [SensorAlert(**kwargs)]
+
+
+@dataclass
+class ScannerRule(System):
+	rule: m.Rule
+	alerts: list[SensorAlert]
+	state_when_pending: State = State.PASSING
+
+	kind: ClassVar[Kind] = Kind("grafana.org/alerts", "alertrule")
+
+	@property
+	def name(self) -> str:
+		return self.rule.name
+
+	def status(self) -> Status:
+		match self.rule.state:
+			case m.RuleState.pending:
+				state = self.state_when_pending
+			case m.RuleState.firing:
+				state = State.FAILING
+			case m.RuleState.inactive:
+				state = State.PASSING
+
+		match state:
+			case State.PASSING:
+				severity = Severity.INFO
+			case State.FAILING:
+				severity = Severity.ERROR
+			case State.UNKNOWN:
+				severity = Severity.WARN
+
+		return Status(state=state, messages=[Log(message=self.rule.state.value, severity=severity)])
+
+	def children(self) -> list[Scanner]:
+		return self.alerts
+
+
+@dataclass
+class InstrumentorAlertRule(Instrumentor):
+	api: GrafanaApi
+
+	def registrations(self) -> Registrations:
+		return [
+			(ScannerRule.kind, self),
+		]
+
+	def instrument(self, registry: InstrumentorRegistry, kind: Kind, **kwargs) -> list[Scanner]:
+		rule = kwargs["rule"]
+		if isinstance(rule, GrafanaObjRef):
+			rule = self.api.get_rule(rule.p["group"], rule.name)
+		elif isinstance(rule, m.Rule):
+			rule = rule
+		else:
+			raise InstrumentorError(f"cannot instantiate ScannerRule for unknown type {type(rule)}")
+		return [ScannerRule(rule, flatten([registry.instrument(SensorAlert.kind, alert=e) for e in rule.alerts]))]
+
+
+@dataclass
+class ScannerGroup(System):
+	group: m.Group
+	rules: list[ScannerRule]
+
+	kind: ClassVar[Kind] = Kind("grafana.org/alerts", "group")
+
+	@property
+	def name(self) -> str:
+		return self.group.name
+
+	status = status_all
+
+	def children(self) -> list[Scanner]:
+		return self.rules
+
+
+@dataclass
+class InstrumentorAlertRuleGroup(Instrumentor):
+	api: GrafanaApi
+
+	def registrations(self) -> Registrations:
+		return [
+			(ScannerGroup.kind, self),
+		]
+
+	def instrument(self, registry: InstrumentorRegistry, kind: Kind, **kwargs) -> list[Scanner]:
+		group = kwargs["group"]
+		if isinstance(group, GrafanaObjRef):
+			group = self.api.get_group(group.name)
+		elif isinstance(group, m.Group):
+			group = group
+		else:
+			raise InstrumentorError(f"cannot instantiate ScannerGroup for unknown type {type(group)}")
+
+		return [ScannerGroup(group, flatten(registry.instrument(ScannerRule.kind, rule=e) for e in group.rules))]
+
+
+@dataclass
+class ScannerGrafana(System):
+	name: str
+	groups: list[ScannerGroup]
+
+	kind: ClassVar[Kind] = Kind("grafana.org/alerts", "grafana")
+
+	status = status_all
+
+	def children(self) -> list[Scanner]:
+		return self.groups
+
+@dataclass
+class InstrumentorGrafana(Instrumentor):
+	api: GrafanaApi
+
+	def registrations(self) -> Registrations:
+		return [
+			(ScannerGrafana.kind, self),
+		]
+
+	def instrument(self, registry: InstrumentorRegistry, kind: Kind, **kwargs) -> list[Scanner]:
+		name = kwargs.get("name", "Grafana")
+
+		groups = self.api.request_alertgroups()
+		return [ScannerGrafana(name, flatten(registry.instrument(ScannerGroup.kind, group=e) for e in groups))]
---
+++ src/main/python/alpacalert/instrumentors/grafana_models.py
@@ -0,0 +1,101 @@
+from datetime import datetime
+from enum import Enum
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel
+from typing_extensions import NotRequired, TypedDict
+
+
+# Define Enums
+class AlertState(str, Enum):
+	normal = "Normal"
+	alerting = "Alerting"
+	pending = "Pending"
+	nodata = "NoData"
+	error = "Error"
+	inactive = "inactive"
+	normal_error = "Normal (Error)"
+	alerting_nodata = "Alerting (NoData)"
+	alerting_error = "Alerting (Error)"
+
+
+class HealthEnum(str, Enum):
+	ok = "ok"
+	error = "error"
+	unknown = "unknown"
+	nodata = "nodata"
+
+
+# Define TypedDicts
+class Labels(TypedDict):
+	alertname: str
+	grafana_folder: str
+	__name__: NotRequired[str]
+
+
+class Annotations(TypedDict): ...
+
+
+class Alert(BaseModel):
+	labels: Labels
+	annotations: Annotations
+	state: AlertState
+	activeAt: datetime
+	value: str
+
+	def name(self):
+		if "__name__" in self.labels:
+			return self.labels["__name__"]
+		elif "alertname" in self.labels:
+			return self.labels["alertname"]
+		else:
+			return "Alert"
+
+
+class Totals(BaseModel):
+	normal: int = 0
+	error: int = 0
+	nodata: int = 0
+	alerting: int = 0
+
+
+class RuleState(str, Enum):
+	"""Rule states are prometheus states"""
+
+	inactive = "inactive"
+	pending = "pending"
+	firing = "firing"
+
+
+class Rule(BaseModel):
+	state: RuleState
+	name: str
+	query: str
+	duration: Optional[int] = 0
+	alerts: List[Alert]
+	totals: Totals
+	totalsFiltered: Totals
+	health: HealthEnum
+	type: str
+	lastEvaluation: datetime
+	evaluationTime: float
+
+
+class Group(BaseModel):
+	name: str
+	file: str
+	rules: List[Rule]
+	totals: Dict[str, int]
+	interval: int
+	lastEvaluation: datetime
+	evaluationTime: float
+
+
+class Data(BaseModel):
+	groups: List[Group]
+	totals: Dict[str, int]
+
+
+class GrafanaAlertsResponse(BaseModel):
+	status: str
+	data: Data
