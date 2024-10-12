6141cebc502aa9cfa633c43453b616c3571a8066
Daniel Goldman <danielgoldman4@gmail.com>
2024-10-12 19:13:38 -0400

    commit models for other interfaces

---
+++ src/main/python/alpacalert/instrumentors/alertmanager_models.py
@@ -0,0 +1,62 @@
+"""Models for AlertManager"""
+# pylint: disable=C0115
+
+from datetime import datetime
+from enum import Enum
+from typing import Any, List, Optional, TypedDict
+
+from pydantic import BaseModel
+
+
+class Annotations(TypedDict):
+	__orgId__: str
+
+
+class Labels(TypedDict):
+	__alert_rule_uid__: str
+	alertname: str
+	grafana_folder: str
+	rulename: str
+	datasource_uid: Optional[str]
+	ref_id: Optional[str]
+
+
+class Receiver(BaseModel):
+	active: Optional[Any] = None
+	integrations: Optional[Any] = None
+	name: str
+
+
+class State(str, Enum):
+	unprocessed = "unprocessed"
+	active = "active"
+	suppressed = "suppressed"
+
+
+class Status(BaseModel):
+	inhibitedBy: List[str]
+	silencedBy: List[str]
+	state: State
+
+
+class Alert(BaseModel):
+	annotations: Annotations
+	endsAt: datetime
+	fingerprint: str
+	receivers: List[Receiver]
+	startsAt: datetime
+	status: Status
+	updatedAt: datetime
+	generatorURL: str
+	labels: Labels
+
+
+class AlertGroupLabels(TypedDict):
+	alertname: str
+	grafana_folder: str
+
+
+class AlertGroup(BaseModel):
+	alerts: List[Alert]
+	labels: AlertGroupLabels
+	receiver: Receiver
---
+++ src/main/python/alpacalert/instrumentors/prometheus_models.py
@@ -0,0 +1,43 @@
+"""Models for Prometheus alerts"""
+# pylint: disable=C0115
+
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional, TypedDict
+
+from pydantic import BaseModel
+
+
+class State(str, Enum):
+	normal = "Normal"
+	alerting = "Alerting"
+	pending = "Pending"
+	nodata = "NoData"
+	error = "Error"
+
+
+class Labels(TypedDict):
+	alertname: str
+	grafana_folder: str
+	__name__: Optional[str]
+
+
+class Annotations(TypedDict):
+	pass
+
+
+class Alert(BaseModel):
+	labels: Labels
+	annotations: Annotations
+	state: State
+	activeAt: datetime
+	value: str
+
+
+class Data(BaseModel):
+	alerts: List[Alert]
+
+
+class PrometheusResponse(BaseModel):
+	status: str
+	data: Data
